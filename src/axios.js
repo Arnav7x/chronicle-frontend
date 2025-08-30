7f64406e5ee11fa3e618ab304c74e898d64f892b
Arnav <arnav777x@gmail.com>
2025-08-30 19:41:33 +0530

    url

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000", // backend URL
+  baseURL: "https://chronicle-backend-tau.vercel.app/", // backend URL
   withCredentials: true,            // send cookies
 });
 
